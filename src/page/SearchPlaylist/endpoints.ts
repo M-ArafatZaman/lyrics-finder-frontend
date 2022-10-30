b763fb3f955f4a5a3c92a7696e4fdfbe2f273572
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-10-30 06:57:37 +0600

    Added new API endpoints

--- src/page/SearchPlaylist/endpoints.ts
+++ src/page/SearchPlaylist/endpoints.ts
@@ -2,3 +2,6 @@ export const API_WEBSITE = "http://localhost:8000";
 export const LOAD_PLAYLIST_ENDPOINT = "/lyrics-finder-api/load-playlist/";
 export const SEARCH_PLAYLIST_ENDPOINT = "/lyrics-finder-api/search-playlist/";
 export const GET_GENIUS_RESPONSE_TIME = "/lyrics-finder-api/get-genius-response-time/";
+export const LOAD_COMPLETE_PLAYLIST_ENDPOINT = "/lyrics-finder-api/load-complete-playlist/";
+export const SCAN_SONG_ENDPOINT = "/lyrics-finder-api/scan-song/";
+export const GET_LYRICS_ENDPOINT = "/lyrics-finder-api/get-lyrics/";
