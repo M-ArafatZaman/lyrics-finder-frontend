5dd2db314c259c07fc53e9396390ea2c7c6df612
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-11-08 08:12:41 +0600

    Edited viewLyricsModal

--- src/page/SearchPlaylist/ViewLyricsModal.tsx
+++ src/page/SearchPlaylist/ViewLyricsModal.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Dialog from '@mui/material/Dialog';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import CloseIcon from '@mui/icons-material/Close';
 
 // Function props
 interface ViewLyricsModalProps {
@@ -18,9 +24,41 @@ function ViewLyricsModal(props: ViewLyricsModalProps) {
     return (
         <Dialog 
             open={modal}
-            onClose={handleClose} 
+            onClose={handleClose}
+            fullWidth={true}
+            maxWidth="lg"
         >
-            <Box></Box>
+            <Box sx={{p: 3}}>
+                {/* Title */}
+                <Box display="flex" flexDirection="row" alignItems="center" >
+                    <MusicNoteIcon/>
+                    <Typography variant="h4" sx={{ml: 1}}>Lyrics</Typography>
+                    <Box flexGrow={1} />
+                    <IconButton onClick={handleClose} ><CloseIcon/></IconButton>
+                </Box>
+                {/* 
+                <Typography variant="caption" color="textSecondary">Source: <Link href={geniusURL} target="_blank">Genius.com</Link></Typography>
+                 */}
+                <Divider sx={{mt: 1, mb: 2}} />
+                {/* Song name */}
+                {/* <Typography variant="h5" textAlign="center" sx={{my: 1}}>{name} - {artists}</Typography>
+                 */} 
+                {/* Lyrics */}
+                {/* <Box sx={{backgroundColor: "#E3E3E3", p: 1}}>
+                    {_LYRICS.map((eachLyrics, i) => {
+                        if (eachLyrics === "") {
+                            return <br/>
+                        } else {
+                            return <Typography key={i}>{eachLyrics}</Typography>
+                        }
+                    })}
+                </Box>
+ */}
+                {/* Close button */}
+                <Divider sx={{my: 1}} />
+                <Button color="primary" onClick={handleClose}>Close</Button>
+                
+            </Box>
         </Dialog>
     )
 };
